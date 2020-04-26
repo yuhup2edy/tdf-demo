513ca5c880cbf658cbbc4187921fe5b167880911
Sriram Venkataramani <yuhup2edy@Srirams-MacBook-Pro.local>
2020-04-26 17:22:59 -0400

    Added checkbox and form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,25 @@ import { Rar } from './rar';
 })
 export class AppComponent {
   title = 'tdf-demo';
+  approverHasError = true;
   approvers= ['Maryanne Frake','Abhijit Mukherjee','Vrushali Dixit','Shane Kushin'];
   types = ['TFS Admin','TFS Reader','TFS Contributor','TFS Build Admin','TFS Branch Delete','TFS Remove Access','Others'];
   applications = ['iClosings','IT Portal','Vendor Management','Settlement Advantage.com','HomeBase','Homebase+','Async/MsgBus',
                   'Title!Snap','TRUE','CORE','RPA','CORE Connect','$AFE','Others','Go2Agent','Sitefinity','Fast Tool','RPA Forms'];
   
   mydate = new Date('05/01/2020');
-  rarModel = new Rar('','TFS','TFS Contributor',this.mydate,1234567890,'$AFE','Main','1.0','KumarPav,ShrivySh','Maryanne Frake','Provide Access');
+  rarModel = new Rar('','TFS','TFS Contributor',this.mydate,1234567890,'$AFE','Main','1.0','KumarPav,ShrivySh','default','Provide Access');
+
+
+  validateApprover(value)
+  {
+    if (value === 'default')
+    {
+      this.approverHasError = true;
+    }
+    else
+    {
+      this.approverHasError = false;
+    }
+  }
 }
